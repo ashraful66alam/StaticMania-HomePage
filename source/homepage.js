fcc60c36705f68efb55ad53f08a5759e8b04b3a6
ashraful66alam <aamorroid66@gmail.com>
2024-07-08 23:16:38 +0600

    js added

---
+++ source/homepage.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+    alert("This page is loaded!");
+    // const line = document.getElementById("room1").addEventListener('click'); // Replace with your element's ID
+
+    document.getElementById("room1").addEventListener("click", function () {
+        document.getElementById("room1").classList.add("big");
+        document.getElementById("room1").classList.remove("small");
+
+        document.getElementById("room2").classList.add("small");
+        document.getElementById("room2").classList.remove("big");
+
+        document.getElementById("room3").classList.add("small");
+        document.getElementById("room3").classList.remove("big");
+    });
+
+    document.getElementById("room2").addEventListener("click", function () {
+        document.getElementById("room2").classList.add("big");
+        document.getElementById("room2").classList.remove("small");
+
+        document.getElementById("room1").classList.add("small");
+        document.getElementById("room1").classList.remove("big");
+
+        document.getElementById("room3").classList.add("small");
+        document.getElementById("room3").classList.remove("big");
+    });
+
+    document.getElementById("room3").addEventListener("click", function () {
+        document.getElementById("room3").classList.add("big");
+        document.getElementById("room3").classList.remove("small");
+
+        document.getElementById("room2").classList.add("small");
+        document.getElementById("room2").classList.remove("big");
+
+        document.getElementById("room1").classList.add("small");
+        document.getElementById("room1").classList.remove("big");
+    });
+
+};

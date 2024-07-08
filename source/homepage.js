3b1c5fb60d4879db4056e3bb090db88af0bc325d
ashraful66alam <aamorroid66@gmail.com>
2024-07-08 23:57:52 +0600

    js move functionality

--- source/homepage.js
+++ source/homepage.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+
+    // select room js 
     document.getElementById("room1").addEventListener("click", function () {
         document.getElementById("room1").classList.add("big");
         document.getElementById("room1").classList.remove("small");
@@ -31,5 +33,24 @@ window.onload = function () {
         document.getElementById("room1").classList.add("small");
         document.getElementById("room1").classList.remove("big");
     });
+
+
+    // move response js
+    document.getElementById("moveLeft").addEventListener("click", function () {
+        const computedStyle = window.getComputedStyle(document.getElementById("responseContainer"));
+        const currentLeft = computedStyle.left;
+        const currentLeftNum = parseFloat(currentLeft.replace(/px$/, ""));
+        const newLeft = currentLeftNum + -456 + "px";
+        document.getElementById("responseContainer").style.left = newLeft;
+    });
+
+    document.getElementById("moveRight").addEventListener("click", function () {
+        const computedStyle = window.getComputedStyle(document.getElementById("responseContainer"));
+        const currentLeft = computedStyle.left;
+        const currentLeftNum = parseFloat(currentLeft.replace(/px$/, ""));
+        const newLeft = currentLeftNum + 456 + "px";
+        document.getElementById("responseContainer").style.left = newLeft;
+    });
+
 
 };
